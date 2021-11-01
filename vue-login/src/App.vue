00d52f0e8d60e1c7679a6b2d4c9ae8be5f402595
Degofelipe <dego_felipe@hotmail.com>
2021-10-31 22:15:21 -0300

    Login

---
+++ vue-login/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <router-view />
+    <div class="container-title">
+      <h1 class="title">Teste Diego</h1>
+      <button class="btn-login" @click="goToLogin">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  computed: {
+    notIsLoginPage() {
+      return this.$route.name !== "login" && this.$route.name !== "register";
+    }
+  },
+
+  methods: {
+    logout() {
+      localStorage.removeItem('authUser');
+      this.$router.push({name: "login"});
+    },
+    goToLogin() {
+      this.$router.push({ name: "login" });
+    },
+  }
+}
+
+</script>
+
+<style>
+.container-title{
+display: flex;
+justify-content: space-between;
+align-items: center;
+background-color: rgb(161, 172, 169);
+}
+
+.title{
+  color: rgb(61, 61, 61);
+  margin-left: 30px;
+  padding-top: 8px;
+
+}
+.btn-login{
+  display: block;
+  margin-right: 30px;
+  border-radius: 30px;
+  width: 100px;
+}
+</style>
